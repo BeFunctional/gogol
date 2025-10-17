56c179e306821f908730534c2c660e3cfb6d3ad2
o1lo01ol1o <tim.pierson@gmail.com>
2025-10-17 13:59:27 +0100

    Fix package qualified Base64 import

--- lib/gogol-core/src/Gogol/Data/Base64.hs
+++ lib/gogol-core/src/Gogol/Data/Base64.hs
@@ -18,7 +18,7 @@ where
 import Control.Lens (Iso', iso)
 import Data.Aeson (FromJSON (..), ToJSON (..))
 import Data.ByteString (ByteString)
-import "base64-bytestring" qualified Data.ByteString.Base64.URL as Base64
+import qualified "base64-bytestring" Data.ByteString.Base64.URL as Base64
 import Data.Hashable
 import qualified Data.Text.Encoding as Text
 import GHC.Generics (Generic)
